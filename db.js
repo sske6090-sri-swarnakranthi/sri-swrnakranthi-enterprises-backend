c231e3e8e0eaf774f551d155e1447c01e49c74de
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-01-26 22:55:35 +0530

    some changed

--- db.js
+++ db.js
@@ -4,9 +4,16 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is missing')
 }
 
+let ssl = { rejectUnauthorized: false }
+
+if (process.env.PG_SSL_CA_B64) {
+  const ca = Buffer.from(process.env.PG_SSL_CA_B64, 'base64').toString('utf8')
+  ssl = { rejectUnauthorized: true, ca }
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl,
   max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000
